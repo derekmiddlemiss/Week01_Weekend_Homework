553a7bf48f517080b4449e8928aacdbbaf40306d
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-06-30 17:22:30 +0100

    Find pets by name added

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -17,3 +17,18 @@ end
 def increase_pets_sold(pet_shop,number)
   pet_shop[:admin][:pets_sold] += number
 end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length()
+end
+
+def pets_by_breed(pet_shop,breed_to_find)
+  pets_found = []
+  for pet in pet_shop[:pets] do
+    pets_found += [ pet ] if pet[:breed] == breed_to_find
+  end
+  return pets_found
+end
+
+
+
